085599320f438456ddc0e688134b8434dd13583a
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-06 12:18:36 -0200

    Add test success factory

--- tests/App/Factory/WithdrawRequestFactoryTest.php
+++ tests/App/Factory/WithdrawRequestFactoryTest.php
@@ -4,7 +4,22 @@ use \App\Factory\WithdrawRequestFactory;
 
 class WithdrawRequestFactoryTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @expectedException UnexpectedValueException
+     * @expectedExceptionMessage Parametro valorSaque não foi passado na URL
+     */
     public function testCreateMethodWhenAnyObjectNotExistShouldReturnAnException()
+    {
+        $request = \Symfony\Component\HttpFoundation\Request::create('/', 'get', ['valorSaque' => '']);
+
+        $withdraw = WithdrawRequestFactory::create($request);
+        $this->assertInstanceOf(
+            'App\\Entity\\Withdraw',
+            $withdraw
+        );
+    }
+
+    public function testCreateMethodWhenAnyObjectShouldReturnAnSuccess()
     {
         $request = \Symfony\Component\HttpFoundation\Request::create('/', 'get', ['valorSaque' => 180]);
 
