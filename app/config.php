04315df83874f83ce23a5977d171160235f085af
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-06 10:35:43 -0200

    Removed configs not used

--- app/config.php
+++ app/config.php
@@ -1,9 +1,5 @@
 <?php
 
 $config = array(
-    'debug' => true,
-    'twig.path' => __DIR__ . '/../src/App/Views',
-    'twig.options' => array(
-        'cache' => __DIR__ . '/cache/twig',
-    ),
+    'debug' => true
 );
