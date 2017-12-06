8c2e605cfd3a737d165fcff41e779564a3b334ce
Kevin Pugliese <kevin.pugliese@clickbus.com>
2017-12-06 07:44:35 -0200

    configs default controller

---
+++ app/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = array(
+    'debug' => true,
+    'twig.path' => __DIR__ . '/../src/App/Views',
+    'twig.options' => array(
+        'cache' => __DIR__ . '/cache/twig',
+    ),
+);
--- web/index.php
+++ web/index.php
@@ -3,11 +3,15 @@
 date_default_timezone_set('America/Sao_paulo');
 
 require_once __DIR__.'/../app/bootstrap.php';
+include __DIR__ . './../app/config.php';
 
-$app = new Silex\Application();
+use App\Providers\RouterServiceProvider;
+use App\Providers\ControllerServiceProvider;
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-});
+$app = new Silex\Application($config);
+
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+$app->register(new ControllerServiceProvider());
+$app->register(new RouterServiceProvider());
 
 $app->run();
